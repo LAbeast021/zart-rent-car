dd85b01cd5ab35327fba874119e60e9b98892c70
LAbeast021 <kiarashhesampour@gmail.com>
2020-01-21 21:38:02 -0800

    add page

--- src/pages/NewCarPage/NewCarPage.jsx
+++ src/pages/NewCarPage/NewCarPage.jsx
@@ -1,9 +1,23 @@
 import React, {Component} from 'react';
+import axios from 'axios'
 
 class NewCarPage extends Component {
+    state = {
+        brand:'',
+        model:'',
+        year:'',
+        seats:'',
+        condition:'',
+        pictures: [],
+        sellerId: this.props.user._id,
+        sellerUsername: this.props.user.username,
+        exColor:'',
+        intColor:'',
+        engine:''
+    }
 
 fileUploaded = e => {
-    console.log(e.target)
+    console.log(e.target.files[0].__proto__)
 };
 
     render () {
